d1e692ec73e1f1d8b065793e0a5f4095d2f1ae4f
tomaszmj <tomasz.m.j.nowak@gmail.com>
2022-12-03 17:22:16 +0100

    solve day3 part2

--- day3/solve2.py
+++ day3/solve2.py
@@ -1,12 +1,44 @@
 #!/usr/bin/python
 
+def priority(n: str) -> int:
+    v = ord(n)
+    if v >= ord("a") and v <= ord("z"):
+        return v - ord("a") + 1
+    if v >= ord("A") and v <= ord("Z"):
+        return v - ord("A") + 27
+    return 0
+
+
+def find_item_in_3_rucksacks(lines) -> str:
+    common_items = set()
+    for line in lines:
+        s = set(line)
+        if not common_items:
+            common_items = s
+        else:
+            common_items.intersection_update(s)
+    if len(common_items) != 1:
+        raise BaseException(f"expected 1 common item, got {common_items}")
+    return common_items.pop()
+
+
 def main():
+    result = 0
+    lines = []
     with open("data.txt", "r") as f:
         for line in f:
             line = line.strip()
-    print(f"done")
+            if not line:
+                continue
+            lines.append(line)
+    for i in range(0, len(lines), 3):
+        try:
+            item = find_item_in_3_rucksacks(lines[i:i+3])
+            result += priority(item)
+        except BaseException as e:
+            print(f"failed to process lines at index {i}: {e}")
+    print(result)
 
 
 if __name__ == "__main__":
     main()
-
