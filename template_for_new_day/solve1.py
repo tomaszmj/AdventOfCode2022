6bcbe13cb190a8f4bf3d0d9dcc43cd4b11028f66
tomaszmj <tomasz.m.j.nowak@gmail.com>
2022-12-26 00:23:02 +0100

    add all_ints_from_str to template_for_new_day

--- template_for_new_day/solve1.py
+++ template_for_new_day/solve1.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+from typing import List
+
+
+def all_ints_from_str(s: str) -> List[int]:
+    numbers = []
+    number_begin = -1
+    for i, c in enumerate(s):
+        isnumeric = c.isdigit() or (c == "-" and i < len(s) - 1 and s[i+1].isdigit())
+        if isnumeric and number_begin == -1:
+            number_begin = i
+        elif not isnumeric and number_begin >= 0:
+            numbers.append(int(s[number_begin:i]))
+            number_begin = -1
+    if number_begin >= 0:
+        numbers.append(int(s[number_begin:len(s)]))
+    return numbers
+
 
 def main():
     with open("data_small.txt", "r") as f:
