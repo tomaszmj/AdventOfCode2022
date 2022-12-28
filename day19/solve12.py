e367a677f465b2b7fe5cf413ab1d3d41f6812d32
tomaszmj <tomasz.m.j.nowak@gmail.com>
2022-12-28 03:26:55 +0100

    optimize day19

--- day19/solve12.py
+++ day19/solve12.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from collections import namedtuple, defaultdict
 
 
-StateFields = ("ore_robots", "clay_robots", "obsidian_robots", "geode_robots", "ore", "clay", "obsidian", "time_left")
+StateFields = ("ore_robots", "clay_robots", "obsidian_robots", "geode_robots", "ore", "clay", "geode", "obsidian", "time_left")
 State = namedtuple("State", StateFields, defaults=(0,)*len(StateFields))
 RobotIndexByResource = {
     "ore": 0,
@@ -60,69 +60,82 @@ class Blueprint:
             for resource, cost in robot_costs.items():
                 i = RobotIndexByResource[resource]
                 self._max_costs[i] = max(self._max_costs[i], cost)
-        self._store: Dict[State, int] = {}
 
-
+    # count_max_geodes uses brutal DFS with good pruning heuristics
     def count_max_geodes(self, time_left: int) -> int:
-        state = State(ore_robots=1, time_left=time_left)
-        result = self._max_geodes(state)
+        state0 = State(ore_robots=1, time_left=time_left)
+        stats = defaultdict(lambda: 0)  # just to show statistics of search pruning etc.
+        result = 0
+        states = [state0]
+        max_geodes_by_time_left = [0] * (state0.time_left + 1)
+        while len(states) > 0:
+            state = states.pop()
+            if state.time_left <= 0:
+                result = max(result, state.geode)
+                max_geodes_by_time_left[0] = max(max_geodes_by_time_left[0], state.geode)
+                stats["out of time"] += 1
+                continue
+            # max_geodes can be reached if we build geode robot each turn.
+            # In this case max geodes count will be a sum of an arithmetic sequence.
+            max_geode_robots = state.geode_robots + state.time_left - 1
+            max_geodes = state.geode + (state.geode_robots + max_geode_robots) * state.time_left // 2
+            # Skip if we already explored a branch with more geodes than optimisitc max geodes count in current state
+            if max_geodes_by_time_left[state.time_left] > max_geodes:
+                stats["max prunes"] += 1
+                continue
+            # If we have so many robots that we can always build geode robot, we can end now
+            if can_always_build(state, self._costs[3]):
+                result = max(result, max_geodes)
+                max_geodes_by_time_left[state.time_left] = max(max_geodes_by_time_left[state.time_left], max_geodes)
+                stats["arithmetic sequence"] += 1
+                continue
+            # Subproblems if we try to build one of the robots:
+            robots_built = 0
+            for robot_index, costs in enumerate(self._costs):
+                robots_count = state[robot_index]
+                if robot_index < 3:  # ore/clay/obsidian
+                    if robots_count >= self._max_costs[robot_index]:
+                        # there is no point in building given robot
+                        continue
+                time_until_building_robot = 0
+                for resource, cost in costs.items():
+                    diff = cost - getattr(state, resource)
+                    if diff <= 0:
+                        continue  # we have enough of given resource to build robot now
+                    miners = state[RobotIndexByResource[resource]]
+                    if diff > 0 and miners == 0:
+                        time_until_building_robot = state.time_left  # it means we will never get resources for that robot
+                        break
+                    time_until_building_robot = max(time_until_building_robot, math.ceil(diff / miners))
+                if time_until_building_robot >= state.time_left:
+                    continue
+                new_robots = [state[i] + 1 if i == robot_index else state[i] for i in range(4)]
+                t = time_until_building_robot + 1
+                new_state = State(
+                    ore_robots=new_robots[0],
+                    clay_robots=new_robots[1],
+                    obsidian_robots=new_robots[2],
+                    geode_robots=new_robots[3],
+                    ore=state.ore + t*state.ore_robots - costs["ore"],
+                    clay=state.clay + t*state.clay_robots - costs["clay"],
+                    obsidian=state.obsidian + t*state.obsidian_robots - costs["obsidian"],
+                    geode=state.geode + t*state.geode_robots,
+                    time_left=state.time_left - t,
+                )
+                states.append(new_state)
+                robots_built += 1
+            # If we cannot build anything until whole time left - end
+            if robots_built == 0:
+                max_geodes = state.geode + state.geode_robots * state.time_left
+                result = max(result, max_geodes)
+                max_geodes_by_time_left[state.time_left] = max(max_geodes_by_time_left[state.time_left], max_geodes)
+                stats["cannot build"] += 1
+        stats_str = ", ".join(f"{key}: {value}" for key, value in stats.items())
+        print(f"{self._number} count_max_geodes({time_left})={result} (stats: {stats_str})")
         return result
 
     def quality_level(self, time_left: int) -> int:
         return self._number * self.count_max_geodes(time_left)
-
-    def _max_geodes(self, state: State) -> int:
-        if state in self._store:
-            return self._store[state]
-        if state.time_left <= 0:
-            self._store[state] = 0
-            return 0
-        geodes = state.geode_robots * state.time_left
-        
-        # If we have so many robots that we can always build geode robot,
-        # we can maximize geodes count by simply building geode robot each turn.
-        # In this case max geodes count will be a sum of an arithmetic sequence.
-        if can_always_build(state, self._costs[3]):
-            max_geode_robots = state.geode_robots + state.time_left - 1
-            optimisitc_max_geodes = (state.geode_robots + max_geode_robots) * state.time_left // 2
-            self._store[state] = optimisitc_max_geodes
-            return optimisitc_max_geodes
-
-        # Subproblems if we try to build one of the robots:
-        for robot_index, costs in enumerate(self._costs):
-            robots_count = state[robot_index]
-            if robot_index < 3:  # ore/clay/obsidian
-                if robots_count >= self._max_costs[robot_index]:
-                    # there is no point in building given robot
-                    continue
-            time_until_building_robot = 0
-            for resource, cost in costs.items():
-                diff = cost - getattr(state, resource)
-                if diff <= 0:
-                    continue  # we have enough of given resource to build robot now
-                miners = state[RobotIndexByResource[resource]]
-                if diff > 0 and miners == 0:
-                    time_until_building_robot = state.time_left  # it means we will never get resources for that robot
-                    break
-                time_until_building_robot = max(time_until_building_robot, math.ceil(diff / miners))
-            if time_until_building_robot >= state.time_left:
-                continue
-            new_robots = [state[i] + 1 if i == robot_index else state[i] for i in range(4)]
-            t = time_until_building_robot + 1
-            new_state = State(
-                ore_robots=new_robots[0],
-                clay_robots=new_robots[1],
-                obsidian_robots=new_robots[2],
-                geode_robots=new_robots[3],
-                ore=state.ore + t * state.ore_robots - costs["ore"],
-                clay=state.clay + t * state.clay_robots - costs["clay"],
-                obsidian=state.obsidian + t * state.obsidian_robots - costs["obsidian"],
-                time_left=state.time_left-t,
-            )
-            geodes = max(geodes, state.geode_robots*t + self._max_geodes(new_state))
-
-        self._store[state] = geodes
-        return geodes
     
     def __str__(self) -> str:
         return f"{self._number}: costs {self._costs}, max costs {self._max_costs}"
