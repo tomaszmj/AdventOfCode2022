d08d84584478ea704901445073c1ecfdb8a99cec
tomaszmj <tomasz.m.j.nowak@gmail.com>
2023-01-14 00:51:26 +0100

    solve day17 part2 - finally

--- day17/solve2.py
+++ day17/solve2.py
@@ -44,60 +44,24 @@ def rock_can_move(
     return True
 
 
-def chamber_rows_eqal(chamber: Set[Tuple[int, int]], row0: int, row1: int) -> bool:
-    any_element_in_chamber = False
-    all_eqal = True
-    for x in range(7):
-        ok0 = (x, row0) in chamber
-        ok1 = (x, row1) in chamber
-        any_element_in_chamber = any_element_in_chamber or ok0 or ok1
-        all_eqal = all_eqal and (ok0 == ok1)
-        if not all_eqal and any_element_in_chamber:
-            return False  # quick return
-    if not any_element_in_chamber:
-        raise BaseException(f"chamber_rows_eqal(chamber, {row0}, {row1} does not make sense - all coordinates are out of chamber")
-    return all_eqal
-
-
-def check_chamber_cycle(chamber: Set[Tuple[int, int]]):
-    end = max(c[1] for c in chamber) + 1
-    max_h, y0_at_max_h, y1_at_max_h = 0, 0, 1
-    for y0 in range(end-1):
-        for y1 in range(y0+1+max_h, end-max_h):
-            h = 0
-            max_current_h = min(y1 - y0 - 1, end - y1 - 1)
-            while h <= max_current_h and chamber_rows_eqal(chamber, y0+h, y1+h):
-                h += 1
-            if h > max_h:
-                max_h = h
-                y0_at_max_h = y0
-                y1_at_max_h = y1
-    print(f"detected chamber shape similarity at y0={y0_at_max_h}, y1={y1_at_max_h}, h={max_h}, chamber height {end}")
-
-
-def check_height_cycle(delta_height: List[int]):
-    end = len(delta_height)
-    max_len, max_i0, max_i1 = 0, 0, 1
-    for i0 in range(end - 1):
-        for i1 in range(i0+1+max_len, end-max_len):
-            ll = 0
-            max_ll = min(i1 - i0 - 1, end - i1 - 1)
-            while ll <= max_ll and delta_height[i0+ll] == delta_height[i1+ll]:
-                ll += 1
-            if ll > max_len:
-                max_len = ll
-                max_i0 = i0
-                max_i1 = i1
-    if max_i0 + max_len == max_i1:
-        print(f"detected height diff similarity at i0={max_i0}, i1={max_i1}, max_len={max_len}, rocks fallen {end}")
-    else:
-        print(f"detected pseudo-height diff similarity at i0={max_i0}, i1={max_i1}, max_len={max_len}, rocks fallen {end}")
+def compute_height(
+    rocks_count: int,
+    heights: List[int],
+    rocks_fallen_until_cycle_begin: int,
+    cycle_period: int,
+) -> int:
+    if rocks_count < len(heights):
+        return heights[rocks_count]
+    cycle_height = heights[-1] - heights[rocks_fallen_until_cycle_begin]
+    offset = rocks_count - rocks_fallen_until_cycle_begin
+    cycles = offset // cycle_period
+    remainder = offset % cycle_period
+    return cycle_height*cycles + heights[rocks_fallen_until_cycle_begin + remainder]
 
 
 def main():
     height = 0
     chamber: Set[Tuple[int, int]] = set()
-    moves = itertools.cycle(INPUT_SMALL)
     rocks_as_sets: List[Set[Tuple[int, int]]] = []
     for rock in ROCKS:
         rock_set = set()
@@ -106,15 +70,24 @@ def main():
                 if r == "#":
                     rock_set.add((rock_x, len(rock)-rock_y)) 
         rocks_as_sets.append(rock_set)
-    rocks = itertools.cycle(rocks_as_sets)
-    delta_height = []
-    last_height = 0
-    for _ in range(len(ROCKS)*len(INPUT_SMALL)*3+15):
-        rock = rocks.__next__()
+    winds = [1 if wind == ">" else -1 for wind in INPUT]
+    floor = [0]*7
+    floor_distance_from_height = (0,)*7
+    states_seen = {}  # key: rock index, wind index, floor_distance_from_height, value: rocks fallen so far
+    wind_index = 0
+    heights = [0]
+    for i, rock in enumerate(itertools.cycle(rocks_as_sets)):
         x, y = 2, height + 3
+        state = (i % len(rocks_as_sets), wind_index) + floor_distance_from_height
+        if state in states_seen:
+            rocks_fallen_until_cycle_begin = states_seen[state]
+            cycle_period = i - rocks_fallen_until_cycle_begin
+            print(f"cycle detected: begin {rocks_fallen_until_cycle_begin}, period {cycle_period}, state {state}")
+            break
+        states_seen[state] = i
         while True:
-            c = moves.__next__()
-            dx = 1 if c == ">" else -1
+            dx = winds[wind_index]
+            wind_index = (wind_index + 1) % len(winds)
             if rock_can_move(chamber, rock, x, y, dx, 0):
                 x += dx
             if rock_can_move(chamber, rock, x, y, 0, -1):
@@ -124,10 +97,12 @@ def main():
         for rx, ry in rock:
             height = max(height, y + ry)
             chamber.add((x + rx, y + ry))
-        delta_height.append(height - last_height)
-        last_height = height
-    check_chamber_cycle(chamber)
-    check_height_cycle(delta_height)
+            floor[x + rx] = max(floor[x + rx], y + ry)
+        floor_distance_from_height = tuple(height - f for f in floor)
+        heights.append(height)
+    print("part1", compute_height(2022, heights, rocks_fallen_until_cycle_begin, cycle_period))
+    print("part2", compute_height(1000000000000, heights, rocks_fallen_until_cycle_begin, cycle_period))
+
 
 
 if __name__ == "__main__":
